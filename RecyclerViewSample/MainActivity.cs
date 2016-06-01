bedbdf926768f4393100f9380873b1c982fe5269
Tirumal <tirumal@uniksys.co.in>
2016-06-01 12:38:51 +0530

    Added Recycler View simple Example with click options

---
+++ RecyclerViewSample/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+
+namespace RecyclerViewSample
+{
+	[Activity (Label = "RecyclerViewSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		RecyclerView recyclerView;
+		RecyclerView.LayoutManager layoutManager;
+		RecyclerAdapter adapter;
+		List<string> items;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			recyclerView = FindViewById<RecyclerView> (Resource.Id.recyclerView);
+			 
+			// improve performance if you know that changes in content
+			// do not change the size of the RecyclerView
+			recyclerView.HasFixedSize = true;
+
+			// use a linear layout manager
+			layoutManager = new LinearLayoutManager (this);
+			recyclerView.SetLayoutManager (layoutManager);
+
+			items = new List<string> ();
+			for (int i = 0; i <= 1000; i++)
+			{
+				var item = string.Format ("Item No {0} #", i+1);
+				items.Add (item);
+			}
+
+			// specify an adapter
+			adapter = new RecyclerAdapter (items.ToArray());
+
+			adapter.ItemClick += Adapter_ItemClick;
+			adapter.ItemLongClick += Adapter_ItemLongClick;
+			recyclerView.SetAdapter (adapter);
+		}
+
+		void Adapter_ItemLongClick (object sender, int e)
+		{
+			adapter.ToggleSelection (e);
+			var position = e + 1;
+			Toast.MakeText (this, "Long Pressed on Item No : " + position, ToastLength.Short).Show ();
+		}
+
+		void Adapter_ItemClick (object sender, int e)
+		{
+			adapter.ToggleSelection (e);
+			var position = e + 1;
+			Toast.MakeText (this, "Clicked on Item No : " + position, ToastLength.Short).Show ();
+		}
+	}
+}
+
+
---
+++ RecyclerViewSample/RecyclerAdapter.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RecyclerView = Android.Support.V7.Widget.RecyclerView;
+using Android.Graphics;
+using Android.Util;
+
+namespace RecyclerViewSample
+{
+	[Activity (Label = "RecyclerAdapter")]			
+	public class RecyclerAdapter : RecyclerView.Adapter
+	{
+		string [] items;
+		public event EventHandler<int> ItemClick;
+		public event EventHandler<int> ItemLongClick;
+		private SparseBooleanArray _SelectedItemIds;
+		private int _position;
+
+		public RecyclerAdapter(string[] data)
+		{
+			items = data;
+			_SelectedItemIds = new SparseBooleanArray();
+		}
+
+		#region implemented abstract members of Adapter
+
+		public override void OnBindViewHolder (RecyclerView.ViewHolder holder, int position)
+		{
+			var item = items [position];
+
+			// Replace the contents of the view with that element
+			var viewHolder = holder as MyViewHolder;
+			viewHolder.TextView.Text = items[position];
+
+			if (_SelectedItemIds.Get(position))
+			{                
+				viewHolder.TextView.SetBackgroundColor(Color.ParseColor("#9EC0FF"));
+			}
+			else
+			{
+				viewHolder.TextView.SetBackgroundColor(Color.Transparent);
+			}
+		}
+
+		public override RecyclerView.ViewHolder OnCreateViewHolder (ViewGroup parent, int viewType)
+		{
+			// set the view's size, margins, paddings and layout parameters
+			var tv = new TextView (parent.Context);
+			LinearLayout.LayoutParams loclayoutparams = new LinearLayout.LayoutParams(RelativeLayout.LayoutParams.MatchParent, 100);
+			tv.LayoutParameters = loclayoutparams;
+			tv.SetTextColor (Color.Black);
+			tv.TextSize = 20;
+			tv.Gravity = GravityFlags.Center;
+
+			tv.LongClick += (sender, e) => 
+			{
+				tv.SetBackgroundColor (Color.ParseColor ("#9EC0FF"));
+				var a = tv.Text;
+			};
+
+			var vh = new MyViewHolder (tv, OnClick, OnLongClick);
+			return vh;
+		}
+
+		public override int ItemCount {
+			get {
+				return items.Length;
+			}
+		}
+
+		#endregion
+
+		void OnClick (int position)
+		{
+			if (ItemClick != null)
+				ItemClick (this, position);
+			_position = position;
+		}
+
+		void OnLongClick (int position)
+		{
+			if (ItemLongClick != null)
+				ItemLongClick (this, position);
+			_position = position;
+		}
+
+		public class MyViewHolder : RecyclerView.ViewHolder
+		{
+			public TextView TextView { get; set; }
+
+
+			public MyViewHolder (TextView v, Action<int> listener, Action<int> longListener) : base (v)
+			{
+				TextView = v;
+				v.Click += (sender, e) => listener (base.Position);
+				v.LongClick += (sender, e) => longListener (base.Position);
+			}
+		}
+
+		public void ToggleSelection(int position)
+		{
+			SelectView(position, !_SelectedItemIds.Get(position));
+		}
+
+		public void SelectView(int position, bool value)
+		{
+			if(value)
+				_SelectedItemIds.Put(position,value);
+			else
+				_SelectedItemIds.Delete(position);
+			NotifyDataSetChanged();
+		}
+	}
+}
+
